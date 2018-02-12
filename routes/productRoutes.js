85e3772c1adfa75eba2e388f903f6e7f9f60bccf
mtslck <mats.loock@lnu.se>
2018-02-12 14:25:28 +0100

    Updating before spring 2018

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,40 +8,18 @@
 'use strict'
 
 const router = require('express').Router()
+const productController = require('../controllers/productController')
 
-// "Faking" persistent products.
-const products = [{id: 1, name: 'Phone'}]
+// /product
+router.get('/', productController.index)
 
-// /
-router.get('/', (req, res) => res.render('product/index', {products}))
-
-// /create
+// /product/create
 router.route('/create')
-  .get((req, res) => res.render('product/create'))
-  .post((req, res) => {
-    // Make the product "persistent" and...
-    products.push({
-      id: products.length + 1,
-      name: req.body.name
-    })
-
-    // ...redirect to the list of products (product/).
-    res.redirect('.')
-  })
+  .get(productController.create)
+  .post(productController.createPost)
 
 // /product/:id
-router.get('/:id', (req, res, next) => {
-  // Get the first product that's id equals the parameter id's value.
-  let product = products.filter(product => product.id === Number(req.params.id)).shift()
-
-  // If no product is found send a 404 (resource not found).
-  if (!product) {
-    return next()
-  }
-
-  // Send response with the wanted product.
-  res.render('product/details', {product})
-})
+router.get('/:id', productController.details)
 
 // Exports.
 module.exports = router
